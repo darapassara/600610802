e73c3c8ef62c742a8a1dae26f3bbb1d2ab792430
darapassara <35342287+darapassara@users.noreply.github.com>
2018-01-12 16:36:56 +0700

    Add files via upload

---
+++ 333333.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, count = 0;
+	string text1, text2;
+	
+	cout << "Enter the first text: ";
+	cin >> text1;
+	cout << "\nEnter the second text: ";
+	cin >> text2;
+	
+	cout << "\nEnter N: ";
+	cin >> N;
+	
+	while(count < N){
+		if(count%2 == 0){
+			cout << text1 << " ";
+		}
+		else{
+			cout << text2 << " ";
+		}
+			count = count + 1;
+	
+	
+	}
+	
+	
+		return 0;
+}
